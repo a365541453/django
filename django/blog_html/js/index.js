9c6b3159089fef0b5a2abbaed4126be3f3d1bac3
a365541453 <365541453@qq.com>
2017-09-18 22:05:07 +0800

    20170918

---
+++ django/blog_html/js/index.js
@@ -0,0 +1,10 @@
+$(function(){
+	$(".contentsul li").click(function(){
+		$(this).children(".ul2").slideToggle(300);
+		console.log("11");
+	});
+//	$(".contents .more").click(function(){
+//		$(this).find("contentsul").css{
+//		}
+//	});
+});
