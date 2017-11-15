1cb4357a47231046c9febe00b33353e03a704c45
a365541453 <32005940+a365541453@users.noreply.github.com>
2017-11-15 17:11:18 +0800

    20171115

--- django/blog/index_part/admin.py
+++ django/blog/index_part/admin.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.contrib import admin
 
 from django import forms
@@ -6,19 +7,31 @@ from django.forms import widgets, models, fields
 # Register your models here.
 from index_part.models import article
 
-
+#form是用来控制显示的，这段代码是用来修改form的默认显示
 class FlatPageForm(forms.ModelForm):
 	class Meta:
 		model = article
 		exclude = []
 		widgets = {
 			"type": fields.Select(choices=[
-				(1,"one"),
-				(2,"two")
+				(1,"主页"),
+				(2,"vmware"),
+				(3,"自动化"),
+				(4,"KVM"),
+				(5,"LInux"),
+				(6,"Docker"),
+				(7,"数据库"),
 			])
 		}
 
 
-@admin.register(article)
+
+#FlatPageForm是form类，不能用来与article一起注册
+#所以要将修改好的form类和admin类关联起来
 class FlatPageAdmin(admin.ModelAdmin):
 	form = FlatPageForm
+
+
+#再将修改后的admin类的方式来注册article
+#article要用admin类的方法来注册
+admin.site.register(article,FlatPageAdmin)
