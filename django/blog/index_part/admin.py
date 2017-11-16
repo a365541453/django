906c17c1c96d3570ca4d6190db45fc2d62acbf27
a365541453 <32005940+a365541453@users.noreply.github.com>
2017-11-16 17:15:55 +0800

    20171116

--- django/blog/index_part/admin.py
+++ django/blog/index_part/admin.py
@@ -9,26 +9,37 @@ from django.forms import  fields
 
 #form是用来控制显示的，这段代码是用来修改form的默认显示
 class FlatPageForm(forms.ModelForm):
-	class Meta:
-		model = index_article
-		exclude = []
-		widgets = {
-			"type": fields.Select(choices=[
-				(1,"主页"),
-				(2,"vmware"),
-				(3,"自动化"),
-				(4,"KVM"),
-				(5,"LInux"),
-				(6,"Docker"),
-				(7,"数据库"),
-			])
-		}
+    class Meta:
+        model = index_article
+        exclude = []
+        widgets = {
+            "type": fields.Select(choices=[
+                (1,"主页"),
+                (2,"vmware"),
+                (3,"自动化"),
+                (4,"KVM"),
+                (5,"LInux"),
+                (6,"Docker"),
+                (7,"数据库"),
+            ])
+        }
+
 
 
 #FlatPageForm是form类，不能用来与article一起注册
 #所以要将修改好的form类和admin类关联起来
 class FlatPageAdmin(admin.ModelAdmin):
-	form = FlatPageForm
+    form = FlatPageForm
+
+    #下面是添加富文本编辑器的代码
+    list_display = ('title','time','type','text')
+    class Media:
+        # 在管理后台的HTML文件中加入js文件, 每一个路径都会追加STATIC_URL/
+        js = (
+            'kindeditor/kindeditor-all.js',
+            'kindeditor/lang/zh_CN.js',
+            'kindeditor/config.js',
+        )
 
 
 #再将修改后的admin类的方式来注册article
--- django/blog/static/kindeditor/config.js
+++ django/blog/static/kindeditor/config.js
@@ -1,7 +1,7 @@
 //config.js
 KindEditor.ready(function(K) {
     //通过浏览器调试查看富文本相关信息，如id，name
-    window.editor = K.create('textarea[name=content]',{
+    window.editor = K.create('textarea[name=text]',{
 
         // 指定大小
         width:'800px',
